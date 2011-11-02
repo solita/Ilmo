fb460a3288b95cc2042b831778748fd1d6a6e155
jvmakine <jvmakine@gmail.com>
2011-11-02 11:02:36 +0200

    Lisättiin koulutukseen kuvaus, järjestäjä ja muut tuiedot -kentät

--- src/main/scala/code/model/Training.scala
+++ src/main/scala/code/model/Training.scala
@@ -1,13 +1,44 @@
 package code.model
 
 import _root_.net.liftweb.mapper._
+import net.liftweb.util._
+import net.liftweb.http._
 
 class Training extends LongKeyedMapper[Training] with IdPK with OneToMany[Long, Training] {
   def getSingleton = Training
 
-  object name extends MappedString(this, 100)
-  object description extends MappedTextarea(this, 2000)
-  object organizer extends MappedString(this, 100)
+  object name extends MappedString(this, 100) {
+    
+    override def validations =  validateGiven _ :: Nil
+
+    def validateGiven(name : String) = {
+      if (organizer.length == 0) {
+        List(FieldError(this, S ? "Name must be given"))
+      } else if (name.length < 5) {
+        List(FieldError(this, S ? "Name is too short"))
+      } else {
+        List[FieldError]()
+      }
+    }
+    
+  }
+
+  object organizer extends MappedString(this, 100) {
+    
+    override def validations =  validateGiven _ :: Nil
+    
+	def validateGiven(organizer : String) = {
+	  if (organizer.length == 0) {
+	    List(FieldError(this, S ? "Organizer must be given"))
+	  } else {
+	    List[FieldError]()
+	  }
+	}
+    
+  }
+  
+  object description extends MappedTextarea(this, 1500)
+  object other extends MappedTextarea(this, 1500)
 
   object participants extends MappedOneToMany(Participant, Participant.training, 
       OrderBy(Participant.id, Ascending))
@@ -25,5 +56,5 @@ class Training extends LongKeyedMapper[Training] with IdPK with OneToMany[Long, 
 }
 
 object Training extends Training with LongKeyedMetaMapper[Training] {
-  override def fieldOrder = List(name)
+  override def fieldOrder = List(name, organizer, description, other)
 }
