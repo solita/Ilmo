ccb8b923d11d77695aa41c7d5f87d7d17f647326
jvmakine <jvmakine@gmail.com>
2011-11-09 11:04:21 +0200

    Pitoajan validointi

--- src/main/scala/code/snippet/AddTrainingSession.scala
+++ src/main/scala/code/snippet/AddTrainingSession.scala
@@ -3,10 +3,15 @@ package code.snippet
 import net.liftweb._
 import http._
 import code.model.{Training, TrainingSession}
+import code.util._
 import net.liftweb.common._
 import net.liftweb.util.FieldError
 import scala.xml.Text
 import java.util.Date
+import net.liftweb.util.FieldIdentifier
+import java.text.SimpleDateFormat
+import java.text.ParseException
+
 
 object AddTrainingSession extends LiftScreen {
   
@@ -16,26 +21,36 @@ object AddTrainingSession extends LiftScreen {
   
   val trainings: List[Box[Training]] = Empty :: Training.findAll().map(t => Full(t))
   
-  var training = select[Box[Training]](S ?? "training-session.training", Empty, trainings)((box: Box[Training]) => box match {
+  // Fields
+  
+  val training = select[Box[Training]](S ?? "training-session.training", Empty, trainings)((box: Box[Training]) => box match {
     case Full(t) => t.name.get
     case default => ""
   })
 
   addFields(() => trainingSession.is.place)
-  addFields(() => trainingSession.is.date)
+  
+  val dateField = text(S ?? "training-session.date", "", 
+	{ s: String => DateUtil.parse(s) match {
+	  case null => FieldError(currentField.box.get, Text(S ?? "training-session.error.training-date-format")) :: Nil
+	  case d if(d.before(new Date)) => FieldError(currentField.box.get, Text(S ?? "training-session.error.training-date-too-early")) :: Nil
+	  case _ => Nil
+	}})
+	
   addFields(() => trainingSession.is.maxParticipants)
+  
+  // Validations
   
   override def validations = validateTrainingSession _ :: super.validations
   
   def validateTrainingSession: Errors = {
     var errors: List[FieldError] = Nil 
     if(training.isEmpty) errors = FieldError(training, Text(S ?? "training-session.error.training-missing")) :: errors
-    if(trainingSession.is.date.get == null) errors = FieldError(trainingSession.is.date, Text(S ?? "training-session.error.training-date-missing")) :: errors
-    else if(trainingSession.is.date.get.before(new Date)) errors = FieldError(trainingSession.is.date, Text(S ?? "training-session.error.training-date-too-early")) :: errors
     errors
   }
     
   def finish() {
+    trainingSession.is.date(DateUtil.parse(dateField.is))
     trainingSession.is.training(training.get)
     trainingSession.is.save
     S.notice(S ?? "training-session.created")
