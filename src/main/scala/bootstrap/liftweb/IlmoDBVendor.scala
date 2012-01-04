8c0a5dfae9fd8079922f1673a637d40fc7524b92
Janne Rintanen <janne.rintanen@gmail.com>
2012-01-04 14:55:07 +0200

    lisätty betamerkintä ja kasvatettu poolsize vielä lisää

--- src/main/scala/bootstrap/liftweb/IlmoDBVendor.scala
+++ src/main/scala/bootstrap/liftweb/IlmoDBVendor.scala
@@ -5,7 +5,7 @@ import net.liftweb.common.Box
 class IlmoDBVendor(driverName : String, dbUrl : String, dbUser : Box[String], dbPassword : Box[String]) extends 
     StandardDBVendor(driverName : String, dbUrl : String, dbUser : Box[String], dbPassword : Box[String])  {
   
-  override def maxPoolSize = 20;
-  override def doNotExpandBeyond = 30
+  override def maxPoolSize = 30;
+  override def doNotExpandBeyond = 40;
 
 }
