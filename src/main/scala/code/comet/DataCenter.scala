9c3aae9b2a4499afa79d41a223330b97086ee727
Joonas Rouhiainen <Joonas.Rouhiainen@solita.fi>
2013-06-28 19:20:32 +0300

    Make StateChanged a sealed trait (was an abstract class), move it to its own file.

--- src/main/scala/code/comet/DataCenter.scala
+++ src/main/scala/code/comet/DataCenter.scala
@@ -17,15 +17,6 @@ import net.liftweb.mapper.By
 import net.liftweb.mapper.Mapper
 import net.liftweb.mapper.Like
 
-abstract class StateChanged
-case object TrainingsChanged extends StateChanged
-case class TrainingSelected(trainingSessionId: Long) extends StateChanged
-case class NewParticipant(name: String, trainingId: Long) extends StateChanged
-case class DelParticipant(name: String, trainingId: Long) extends StateChanged
-case class UserSignedIn(name: String) extends StateChanged
-case class UserSignedOut(name: String) extends StateChanged
-case object Init extends StateChanged
-
 
 // FIXME: muuta nimi esim. IlmoListenerManager
 object DataCenter extends LiftActor with ListenerManager {
